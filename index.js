cb5728bdabcfb1532df8a83ecf8c5048c88c60de
Kolya Evtikhovich <evklaus@gmail.com>
2020-12-27 21:50:04 +0300

    feature: add netlify deps

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
-const PORT = 3000;
+const bodyParser = require('body-parser');
+const router = express.Router();
 
-app.use(express.static(path.join(__dirname, 'static')));
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+router.get('/', (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>Hello World</h1>');
+    res.end();
 });
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);  // path must route to lambda
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'static', 'login.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
---
+++ server-local.js
@@ -0,0 +1,4 @@
+const app = require('./index');
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Local app listening on port ${PORT}!`));
