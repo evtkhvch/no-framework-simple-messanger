16eb0a907455153b60e721c232b512f028e7e073
Kolya Evtikhovich <kolya@redstream.by>
2021-02-16 14:54:43 +0300

    feature: dialog style

--- src/pages/chat/components/dialog/dialog.js
+++ src/pages/chat/components/dialog/dialog.js
@@ -7,18 +7,20 @@ export class Dialog extends Component {
     render() {
         return `
             <dialog id="dialog" class="modal-dialog">
-              <form method="dialog">
-                <p><label>Favorite animal:
-                  <select>
-                    <option></option>
-                    <option>Brine shrimp</option>
-                    <option>Red panda</option>
-                    <option>Spider monkey</option>
-                  </select>
-                </label></p>
-                <menu>
-                  <button value="cancel">Cancel</button>
-                </menu>
+              <form class="modal-dialog__form" method="dialog">
+                <div class="modal-dialog__content">
+                     <div class="modal-dialog__title">Добавить чат</div>
+                     <div class="modal-dialog__input-wrap">
+                          <div class="modal-dialog__input form-item login__field">
+                            <input name="login" type="text" id="login" placeholder="&nbsp;">
+                            <label for="login" data-label="Название"></label>
+                            <span class="error-message"></span>
+                         </div>
+                     </div>
+                </div>
+                <div class="modal-dialog__footer">
+                    <button class="modal-dialog__submit default-button" value="cancel">Добавить</button>
+                </div>
               </form>
             </dialog>
         `;
